828951c7560dd5d0c8bb4d4f9130a3bea4cdaec9
Jaime Baldin <jaimedbaldin@gmail.com>
2021-11-27 23:15:59 -0300

    Created the path to orders pages on man activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGoCompany, btnGoProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGoCompany = findViewById(R.id.btnGoCompany);
+        btnGoProduct = findViewById(R.id.btnGoProduct);
+
+        btnGoProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClientActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
