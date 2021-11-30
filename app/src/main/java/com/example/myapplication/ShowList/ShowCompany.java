7155d62718567f46efe4c0300ab75474df862134
Jaime Baldin <jaimedbaldin@gmail.com>
2021-11-29 21:45:09 -0300

    Add new Classes to show from API

---
+++ app/src/main/java/com/example/myapplication/ShowList/ShowCompany.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.ShowList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowCompany extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_company);
+    }
+}
